fbbd47913733fb3bcd31881b88b329b7714f4c35
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2018-04-07 10:07:37 -0300

    10/06

--- app01/src/app/app.module.ts
+++ app01/src/app/app.module.ts
@@ -3,19 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
-import { TentativasComponent } from './tentativas/tentativas.component';
 import { ProgressoComponent } from './progresso/progresso.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { TopoComponent } from './topo/topo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent,
     PainelComponent,
+    ProgressoComponent,
     TentativasComponent,
-    ProgressoComponent
+    TopoComponent
   ],
   imports: [
     BrowserModule
